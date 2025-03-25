c806bfbd2ee4ea7942529d216ad9a93ba12a1604
veron <lucas.veron@etu.u-paris.fr>
2025-03-25 10:32:14 +0100

    decoupage de messages sur 128 bits

--- aes.py
+++ aes.py
@@ -1,7 +1,10 @@
 import cryptography
 
 def decoupe (m):
-    return [message[i * 128 : (i+1) * 128] for i in range ()]
+    n=len(m)//128
+    message =  [m[i * 128 : (i+1) * 128] for i in range (n)]
+    if n%128 == 0:
+        message [n] = int_to_bytes(pow(2, 128))
 
 def enchiffre (m):
     
